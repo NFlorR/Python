32e2091f29cb52cece65b64970992b894a742b35
NFlorR <ramayo.flor@gmail.com>
2023-07-21 19:52:31 -0300

    commit inicial

---
+++ js.js
@@ -0,0 +1,8 @@
+const input = document.querySelector('input');
+const boton = document.querySelector('btn');
+const listas = document.querySelector('lists');
+
+boton.addEventListener("click", function() {
+    input.value=""
+    listas.appendChild(listItem)
+});
