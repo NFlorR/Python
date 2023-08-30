80373f912be43c1ffca3a7938f92ceaae0ac1760
NFlorR <108238530+NFlorR@users.noreply.github.com>
2023-08-30 09:01:45 -0300

    Delete js.js

--- js.js
+++
@@ -1,8 +0,0 @@
-const input = document.querySelector('input');
-const boton = document.querySelector('btn');
-const listas = document.querySelector('lists');
-
-boton.addEventListener("click", function() {
-    input.value=""
-    listas.appendChild(listItem)
-});
